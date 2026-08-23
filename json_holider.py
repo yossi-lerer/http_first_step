9a9a8071018cd5d9b459b47d7723fb682e86b08a
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-08-23 17:53:16 +0300

    create the base implonment

--- json_holider.py
+++ json_holider.py
@@ -1,6 +1,8 @@
 import json
 
 def write_json(data):
+    if type(data) != list:
+        data = [data]
     json_str = json.dumps(data, indent=4)
     with open("data.json", "w") as f:
         f.write(json_str)
@@ -8,4 +10,7 @@ def write_json(data):
 def get_json():
     with open("data.json", "r") as file:
         data = json.load(file)
+    if data == []:
+        return False
     return data
+
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from weatherapi import get_weather
 from json_holider import get_json, write_json
 from input import userinput
+from check_func import check_cashing
 
 def main():
     city_name = userinput()
-    weather = get_weather(city_name)
-    write = write_json(weather)
-
+    if get_json() != False:
+        get = get_json()
+        check = check_cashing(city_name, get)
+        print(check)
+        if check == False:
+            weather = get_weather(city_name)
+            print(weather)
+            get.append(weather)
+            write = write_json(get)
+    else:
+        get = get_json()
+        weather = get_weather(city_name)
+        print(weather)
+        write = write_json(weather)
 main()
